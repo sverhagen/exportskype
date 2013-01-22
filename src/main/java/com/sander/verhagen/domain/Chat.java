bd218cc3bf2e42805223d955d8255c14f798727a
Sander Verhagen <sanderverh>
2013-01-22 14:44:50 -0800

    Updated Javadoc

--- src/main/java/com/sander/verhagen/domain/Chat.java
+++ src/main/java/com/sander/verhagen/domain/Chat.java
@@ -148,7 +148,14 @@ public class Chat
     {
         return !homeUser.equals(author);
     }
-    
+
+    /**
+     * Whether this chat has any messages at all. Shortcut for <code>isEmpty()</code> on
+     * {@link #getMessages()}
+     * 
+     * 
+     * @return whether any messages
+     */
     public boolean isEmpty()
     {
         return messages.isEmpty();
